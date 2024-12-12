886561deedd2a594f6194466a225e5aace258b9c
Lucas Santos <ls.corporativo18@gmail.com>
2024-12-12 15:01:08 -0300

    Aprendo Playwright

---
+++ tests/login.spec.js
@@ -0,0 +1,43 @@
+const { test, expect } = require('@playwright/test');
+const { customLogin } = require('../support/commands');
+
+// teste sem comando personalizado
+// test('Deve realizar login com sucesso', async ({ page }) => {
+
+//   await page.goto('https://automationpratice.com.br/');
+//   await page.getByRole('link', { name: ' Login' }).click();
+//   await page.locator('#user').fill('aluno@teste.com');
+//   await page.locator('#password').fill('teste123');
+//   await page.getByRole('button', { name: 'login' }).click();
+//   await expect(page.getByText('Olá, aluno@teste.com')).toBeVisible();
+//   await page.getByRole('button', { name: 'OK' }).click();
+
+//  });
+
+test('Deve realizar login com sucesso usando comando personalizado', async ({ page }) => { 
+
+  await customLogin(page, 'aluno@teste.com', 'teste123'); 
+  await expect(page.getByText('Olá, aluno@teste.com')).toBeVisible(); 
+  await page.getByRole('button', { name: 'OK' }).click(); 
+
+});
+
+//teste sem comando personalizado
+// test('Deve apresentar mensagem de erro ao inserir credencial inváldida', async ({ page }) => {
+
+//     await page.goto('https://automationpratice.com.br/');
+//     await page.getByRole('link', { name: ' Login' }).click();
+//     await page.locator('#user').fill('aluno@@teste.com'); //credencial errada
+//     await page.locator('#password').fill('testee0123'); //credencial errada
+//     await page.getByRole('button', { name: 'login' }).click();
+//     await expect(page.getByText('E-mail inválido.')).toBeVisible();
+
+// });
+
+test('Deve apresentar mensagem de erro ao inserir credencial inváldida', async ({ page }) => {
+
+  await customLogin(page, 'aluno@@teste.com', 'testee0123'); //credenciais erradas
+  await expect(page.getByText('E-mail inválido.')).toBeVisible();
+  
+});
+
